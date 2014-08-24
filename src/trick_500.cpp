7aaef8980d8cec375ce5050e3ec429cafbfbd3f8
Nathanael Gray <nathanael@nzgray.com>
2014-08-24 14:21:48 +1200

    Split long line into two.

--- src/trick_500.cpp
+++ src/trick_500.cpp
@@ -9,7 +9,12 @@ Trick_500::Trick_500()
 Game_500::player_t Trick_500::winner(Game_500::suit_t trumps) const
 {
    if (cards.size() != max_size()) throw std::runtime_error("Cannot calculate winner of unfinished trick.");
-   return (Game_500::player_t)((leader + std::max_element(cards.begin(), cards.end(), win_number_compare(trumps, led_suit(trumps))) - cards.begin()) % 4);
+
+   auto winning_card_iterator = std::max_element(cards.begin(),
+                                                 cards.end(),
+                                                 win_number_compare(trumps, led_suit(trumps)));
+
+   return (Game_500::player_t)((leader + winning_card_iterator - cards.begin()) % 4);
 }
 
 Card::suit_t Trick_500::led_suit(Game_500::suit_t trumps) const
