71989359a26f04bf87b1ca9ec4d02961a836a530
Nathanael Gray <nathanael@nzgray.com>
2014-08-24 13:58:01 +1200

    Remove the other giant lookup table (clean code is prefereable over super fast at this stage and it only made about 10% improvement). Add none to game suit enum.

--- src/game_500.h
+++ src/game_500.h
@@ -8,7 +8,7 @@ class Game_500
 {
 public:
    enum variant_t {two_player, two_player_dummy, three_player, three_player_dummy, four_player, five_player};
-   enum suit_t {hearts, diamonds, clubs, spades, no_trumps, misere};
+   enum suit_t {none, hearts, diamonds, clubs, spades, no_trumps, misere};
    enum player_t {player_1, player_2, player_3, player_4};
 };
 
